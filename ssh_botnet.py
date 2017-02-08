3db5afc725b6ab0b3b1ca913c43831250023ae2d
krhertz <krhertz@users.noreply.github.com>
2017-02-07 22:45:22 -0600

    ssh_botnet.py

--- ssh_botnet.py
+++ ssh_botnet.py
@@ -2,7 +2,7 @@
   SSH-BOT
   
  * Author Krhertz 2017
- *
+ 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
